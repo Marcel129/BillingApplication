580eac4571ea2e3e43ceed920410f55794e5fc46
Marcel Skrok <marcel.skrok@onet.pl>
2022-08-24 18:41:37 +0200

    Saving data after work

---
+++ Actualizer/actualizer.cpp
@@ -0,0 +1,7 @@
+#include <windows.h>
+#include <iostream>
+
+int main(){
+    std::cout << "Aktualizacja w trakcie, zaczekaj...."<<std::endl;
+    system("git pull https://github.com/Marcel129/SmallBA_Deployment");
+}
---
+++ Application_build/Release/release/qmlcache_loader.cpp
@@ -0,0 +1,148 @@
+#include <QtQml/qqmlprivate.h>
+#include <QtCore/qdir.h>
+#include <QtCore/qurl.h>
+
+namespace QmlCacheGeneratedCode {
+namespace _0x5f__LineEdit_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__InvElem_bck_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__OptionsScreen_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__main_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__ChooseField_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__MenuBar_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__InvoiceTableHeader_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__PlantsScreen_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__BarButton_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__InvFooter_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__ClientsScreen_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__InvHeaderElem_bck_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__RegisterScreen_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__InvoiceScreen_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+
+}
+namespace {
+struct Registry {
+    Registry();
+    ~Registry();
+    QHash<QString, const QQmlPrivate::CachedQmlUnit*> resourcePathToCachedUnit;
+    static const QQmlPrivate::CachedQmlUnit *lookupCachedUnit(const QUrl &url);
+};
+
+Q_GLOBAL_STATIC(Registry, unitRegistry)
+
+
+Registry::Registry() {
+        resourcePathToCachedUnit.insert(QStringLiteral("/LineEdit.qml"), &QmlCacheGeneratedCode::_0x5f__LineEdit_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/InvElem_bck.qml"), &QmlCacheGeneratedCode::_0x5f__InvElem_bck_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/OptionsScreen.qml"), &QmlCacheGeneratedCode::_0x5f__OptionsScreen_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/main.qml"), &QmlCacheGeneratedCode::_0x5f__main_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/ChooseField.qml"), &QmlCacheGeneratedCode::_0x5f__ChooseField_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/MenuBar.qml"), &QmlCacheGeneratedCode::_0x5f__MenuBar_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/InvoiceTableHeader.qml"), &QmlCacheGeneratedCode::_0x5f__InvoiceTableHeader_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/PlantsScreen.qml"), &QmlCacheGeneratedCode::_0x5f__PlantsScreen_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/BarButton.qml"), &QmlCacheGeneratedCode::_0x5f__BarButton_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/InvFooter.qml"), &QmlCacheGeneratedCode::_0x5f__InvFooter_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/ClientsScreen.qml"), &QmlCacheGeneratedCode::_0x5f__ClientsScreen_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/InvHeaderElem_bck.qml"), &QmlCacheGeneratedCode::_0x5f__InvHeaderElem_bck_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/RegisterScreen.qml"), &QmlCacheGeneratedCode::_0x5f__RegisterScreen_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/InvoiceScreen.qml"), &QmlCacheGeneratedCode::_0x5f__InvoiceScreen_qml::unit);
+    QQmlPrivate::RegisterQmlUnitCacheHook registration;
+    registration.version = 0;
+    registration.lookupCachedQmlUnit = &lookupCachedUnit;
+    QQmlPrivate::qmlregister(QQmlPrivate::QmlUnitCacheHookRegistration, &registration);
+}
+
+Registry::~Registry() {
+    QQmlPrivate::qmlunregister(QQmlPrivate::QmlUnitCacheHookRegistration, quintptr(&lookupCachedUnit));
+}
+
+const QQmlPrivate::CachedQmlUnit *Registry::lookupCachedUnit(const QUrl &url) {
+    if (url.scheme() != QLatin1String("qrc"))
+        return nullptr;
+    QString resourcePath = QDir::cleanPath(url.path());
+    if (resourcePath.isEmpty())
+        return nullptr;
+    if (!resourcePath.startsWith(QLatin1Char('/')))
+        resourcePath.prepend(QLatin1Char('/'));
+    return unitRegistry()->resourcePathToCachedUnit.value(resourcePath, nullptr);
+}
+}
+int QT_MANGLE_NAMESPACE(qInitResources_qml)() {
+    ::unitRegistry();
+    Q_INIT_RESOURCE(qml_qmlcache);
+    return 1;
+}
+Q_CONSTRUCTOR_FUNCTION(QT_MANGLE_NAMESPACE(qInitResources_qml))
+int QT_MANGLE_NAMESPACE(qCleanupResources_qml)() {
+    Q_CLEANUP_RESOURCE(qml_qmlcache);
+    return 1;
+}
